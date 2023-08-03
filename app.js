067213ed4ad956dde877ed58ea4bcab2e3e5b981
Bilawal-Haider <117254325+Bilawal-Haider@users.noreply.github.com>
2023-08-03 15:20:35 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+/*--------PortFolio-----------*/
+
+// Section1
+
+
+var  Section1_Image = document.getElementById("Section1-Image");
+Section1_Image.addEventListener("click", function(e){
+    e.preventDefault;
+
+    Section1_Image.classList.remove("run-animation2");
+    void Section1_Image.offsetWidth;
+
+    Section1_Image.classList.add("run-animation2");
+}, false);
+
+
+// Section1
+
+
+/*--------PortFolio-----------*/  
